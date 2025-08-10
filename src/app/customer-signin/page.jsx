b5d0e47bb5cf1419033710b72bc2936bdb87b7a5
Jimmy Yakubu <jimiyaks3@gmail.com>
2025-08-10 14:34:44 +0100

    removed page

---
+++ src/app/customer-signin/page.jsx
@@ -0,0 +1,13 @@
+
+export default function Login() {
+  
+  return (
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="border p-5 rounded w-100" style={{ maxWidth: 400 }}>
+        <h4 className="mb-4 text-center">Customer Login</h4>
+
+       
+      </div>
+    </div>
+  );
+}
---
+++ src/app/customer-signup/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Signup() {
+  return (
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="border p-5 rounded w-100" style={{ maxWidth: 500 }}>
+        <h4 className="mb-4 text-center">Sign Up</h4>
+      </div>
+    </div>
+  );
+}
